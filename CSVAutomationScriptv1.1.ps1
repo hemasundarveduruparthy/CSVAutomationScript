70167772e1d4c0bea11a181358e9ae651e0d8355
hemasundarveduruparthy <63839652+hemasundarveduruparthy@users.noreply.github.com>
2020-04-20 08:56:15 +0530

    v1.1
    Updated the code in line no.21 by adding TrimEnd code for timing "_"

---
+++ CSVAutomationScriptv1.1.ps1
@@ -0,0 +1,31 @@
+﻿###################################################
+# CSV Automation Script                           #
+# Version     : v1.1                              #
+# Developed by: hemasundar@gmail.com              #
+# Date        : 20.04.2020                        #
+###################################################
+
+$csvfinaldata = @()
+$csvdatapath = "$env:USERPROFILE\Desktop\Siva\test.txt"
+$csvdata = Get-content $csvdatapath
+$outputdate = get-date -f yyyy-MM-dd
+foreach ($line in $csvdata) {
+    if($line -match '\d{2}.\d{2}.\d{4}')  {
+    $data = $line.Split()
+    $data
+    $csvfinaldata += New-Object PSObject -Property ([ordered]@{
+        
+        Date = [string]$data[0]
+        'Sr.No.' = ""
+        'Main Chain' = ''
+        'Sub Chain Name' = [string]$data[4].TrimEnd("_")
+        'IP/DTP/ABAP' = [string]$data[5]
+        'Source System' = ''
+        'Error Message' = ''
+        Remarks = ''
+        Resource = ''
+        })
+
+    }
+}
+$csvfinaldata | export-csv -Path "$env:USERPROFILE\Desktop\Report_$outputdate.csv" -NoTypeInformation
